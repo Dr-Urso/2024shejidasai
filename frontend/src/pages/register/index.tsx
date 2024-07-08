7bc0c9dc8a87f8f18e203eb40e06b0ac5f8eb7af
chunniuma <wurutian14533@outlook.com>
2024-07-08 11:54:38 +0800

    注册2.0

--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -1,54 +1,127 @@
-import React, { useState, useEffect } from 'react';
-import { Button, TextInput } from "@carbon/react";
+import React, { useState, useEffect} from 'react';
+import { Button, TextInput,RadioButtonSkeleton,} from "@carbon/react";
+import {RadioButton, RadioButtonGroup} from "carbon-components-react";
+import {useNavigate} from "react-router-dom";
 import axios from 'axios';
 import styles from './index.less';
-
+import {Link} from "umi";
 export default function Page() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [id, setId] = useState("");
+    const [user_type, setUser_type] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
+    let student_id='';
+    let employee_id='';
     const handleRegister = async () => {
-        try {
-            const response = await fetch('/api/user/create', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'  // 告诉服务器请求体的内容类型是JSON
-                },
-                body: JSON.stringify({ username, password })  // 将对象转换为JSON字符串
-            });
+        if (!username && !password && !nickname && !id && !user_type) {
+            if(user_type=='teacher')
+            {employee_id=id
+                try {
+                    const response = await fetch('/api/user/create', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'  // 告诉服务器请求体的内容类型是JSON
+                        },
+                        body: JSON.stringify({username, password,user_type,employee_id})  // 将对象转换为JSON字符串
+                    });
 
-            const data = await response.json();  // 解析响应中的JSON数据
-            if (response.ok) {
-                console.log('Registration successful:', data);
-            } else {
-                setErrorMessage(`注册失败: ${data.detail}`);
+                    const data = await response.json();  // 解析响应中的JSON数据
+                    if (response.ok) {
+                        console.log('Registration successful:', data);
+                        alert('注册成功，请登录');
+                        navigate('/login');
+                    } else {
+                        setErrorMessage(`注册失败: ${data.detail}`);
+                    }
+                } catch (error) {
+                    console.error('Error during registration:', error);
+                }
             }
-        } catch (error) {
-            console.error('Error during registration:', error);
-        }
+            else{student_id=id
+                try {
+                    const response = await fetch('/api/user/create', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'  // 告诉服务器请求体的内容类型是JSON
+                        },
+                        body: JSON.stringify({username, password,user_type,student_id})  // 将对象转换为JSON字符串
+                    });
+
+                    const data = await response.json();  // 解析响应中的JSON数据
+                    if (response.ok) {
+                        console.log('Registration successful:', data);
+                        alert('注册成功，请登录');
+                        navigate('/login');
+                    } else {
+                        setErrorMessage(`注册失败: ${data.detail}`);
+                    }
+                } catch (error) {
+                    console.error('Error during registration:', error);
+                }
+            }
+
+        } else {
+            setErrorMessage("请填写所有必填项");}
     };
 
     return (
+        <>
         <div className={styles.Container}>
             <div className={styles.Tab}>
-                <h1>欢迎注册未来教育平台</h1>
+                <h1>创建注册未来教育平台账户</h1>
+                <div className={styles.Jump}>
+                    <p>已有账户？</p>
+                    <Link to="/login">登录</Link>
+                </div>
+                <div className={styles.Input}>
                 <TextInput
                     className={styles.Text}
                     id="text-input-1"
                     labelText="邮箱地址"
-                    helperText="反馈信息"
+                    helperText="不能超过16位"
                     onChange={(e) => setUsername(e.target.value)}
                 />
                 <TextInput
                     className={styles.Text}
                     id="text-input-2"
                     labelText="密码"
-                    helperText="反馈信息"
+                    helperText="不能超过16位"
                     onChange={(e) => setPassword(e.target.value)}
                 />
+                    <TextInput
+                    className={styles.Text}
+                    id="text-input-3"
+                    labelText="昵称"
+                    helperText=""
+                    onChange={(e) => setNickname(e.target.value)}
+                />
+                <TextInput
+                    className={styles.Text}
+                    id="text-input-4"
+                    labelText="ID"
+                    helperText=""
+                    onChange={(e) => setId(e.target.value)}
+                />
+                </div>
+                <RadioButtonGroup legendText="您是老师还是学生" name="radio-button-default-group">
+                <RadioButton labelText="老师" value="radio-1" id="radio-1" onClick={()=>setUser_type('teacher')} />
+                <RadioButton labelText="学生" value="radio-2" id="radio-2" onClick={()=>setUser_type('student')}/>
+                </RadioButtonGroup>
                 {errorMessage && <div style={{ color: 'red', marginBottom: '10px' }}>{errorMessage}</div>}
                 <Button className={styles.Button} onClick={handleRegister}>立即注册</Button>
+
+            </div>
+            <div className={styles.Header}>
+                <h1>欢迎注册未来教育平台</h1>
+                <h2>创建账户以使用未来教育平台</h2>
             </div>
         </div>
+    <div className={styles.bottomBorder}>
+        这是底边
+    </div>
+        </>
     );
 }
