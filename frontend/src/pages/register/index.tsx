bb47d846bdeb9649afb929d64fa6235fc17a4438
DrKuma <scy09105@gmail.com>
2024-07-08 13:32:41 +0800

    改了一下注册界面

--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 import axios from 'axios';
 import styles from './index.less';
 import {Link} from "umi";
+import bg from "@/assets/register.svg";
 export default function Page() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -69,59 +70,75 @@ export default function Page() {
 
     return (
         <>
-        <div className={styles.Container}>
-            <div className={styles.Tab}>
-                <h1>创建注册未来教育平台账户</h1>
-                <div className={styles.Jump}>
-                    <p>已有账户？</p>
-                    <Link to="/login">登录</Link>
-                </div>
-                <div className={styles.Input}>
-                <TextInput
-                    className={styles.Text}
-                    id="text-input-1"
-                    labelText="邮箱地址"
-                    helperText="不能超过16位"
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <TextInput
-                    className={styles.Text}
-                    id="text-input-2"
-                    labelText="密码"
-                    helperText="不能超过16位"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                    <TextInput
-                    className={styles.Text}
-                    id="text-input-3"
-                    labelText="昵称"
-                    helperText=""
-                    onChange={(e) => setNickname(e.target.value)}
-                />
-                <TextInput
-                    className={styles.Text}
-                    id="text-input-4"
-                    labelText="ID"
-                    helperText=""
-                    onChange={(e) => setId(e.target.value)}
-                />
-                </div>
-                <RadioButtonGroup legendText="您是老师还是学生" name="radio-button-default-group">
-                <RadioButton labelText="老师" value="radio-1" id="radio-1" onClick={()=>setUser_type('teacher')} />
-                <RadioButton labelText="学生" value="radio-2" id="radio-2" onClick={()=>setUser_type('student')}/>
-                </RadioButtonGroup>
-                {errorMessage && <div style={{ color: 'red', marginBottom: '10px' }}>{errorMessage}</div>}
-                <Button className={styles.Button} onClick={handleRegister}>立即注册</Button>
 
+            <div className={styles.Container}>
+                <div className={styles.illustration_canva} style={{backgroundImage: `url(${bg})`}}></div>
+                <div className={styles.head}>
+
+                </div>
+                <div className={styles.formArea}>
+                    <h1>创建账户</h1>
+                    <div className="h-5"/>
+                    <div className={styles.Jump}>
+                        已有账户？
+                        <Link to="/login">登录</Link>
+                    </div>
+                    <div className="h-5"/>
+                    <div className={styles.Input}>
+                        <TextInput
+                            className={styles.Text}
+                            id="text-input-1"
+                            labelText="邮箱地址"
+                            helperText="不能超过16位"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <div className="h-5"/>
+                        <TextInput
+                            className={styles.Text}
+                            id="text-input-2"
+                            labelText="密码"
+                            helperText="不能超过16位"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <div className="h-5"/>
+                        <TextInput
+                            className={styles.Text}
+                            id="text-input-3"
+                            labelText="昵称"
+                            helperText=""
+                            onChange={(e) => setNickname(e.target.value)}
+                        />
+                        <div className="h-5"/>
+                        <TextInput
+                            className={styles.Text}
+                            id="text-input-4"
+                            labelText="ID"
+                            helperText=""
+                            onChange={(e) => setId(e.target.value)}
+                        />
+                        <div className="h-5"/>
+                    </div>
+                    <RadioButtonGroup legendText="您是老师还是学生" name="radio-button-default-group">
+                        <RadioButton labelText="老师" value="radio-1" id="radio-1"
+                                     onClick={() => setUser_type('teacher')}/>
+                        <RadioButton labelText="学生" value="radio-2" id="radio-2"
+                                     onClick={() => setUser_type('student')}/>
+                    </RadioButtonGroup>
+                    <div className="h-5"/>
+                    {errorMessage && <div style={{color: 'red', marginBottom: '10px'}}>{errorMessage}</div>}
+                    <Button className={styles.Button} onClick={handleRegister}>立即注册</Button>
+
+                </div>
+                <div style={{position: 'absolute', left: '3%', top: '6em'}}>
+                    <h1>欢迎访问未来教育平台</h1>
+                    <div className="h-10"/>
+                    <h2>创建账户以访问平台服务</h2>
+                </div>
+                <div className={styles.bottomBorder}>
+                    这是底边
+                </div>
             </div>
-            <div className={styles.Header}>
-                <h1>欢迎注册未来教育平台</h1>
-                <h2>创建账户以使用未来教育平台</h2>
-            </div>
-        </div>
-    <div className={styles.bottomBorder}>
-        这是底边
-    </div>
+
         </>
     );
 }
